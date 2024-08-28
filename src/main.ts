bfe3ce80d25658b88a5874b53ccad3e9c3f61014
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-08-28 14:05:26 +0100

    Defined game board state and player

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import './styles/styles.css'
+
+
+// Cell options can be 'baguette', 'bagel' or null:
+
+type Player = 'baguette' | 'bagel' | null;
+type GameState = Player[]; // an array of 9 elements - one for each cell
+
+
+// Initialise empty board
+const gameState: GameState = Array(9).fill(null); 
+
+// First player to start - baguette ie. X
+
+let currentPlayer: Player = 'baguette'; 
